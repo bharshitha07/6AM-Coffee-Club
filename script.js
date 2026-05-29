da47a0420ad8ac55be679b18bd0a02a013e79f95
B Harshitha <reddyharshi222@gmail.com>
2026-05-29 10:43:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+
+const cards = document.querySelectorAll(".recipe-card");
+
+window.addEventListener("scroll", () => {
+
+  cards.forEach((card) => {
+
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < window.innerHeight - 100){
+
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+
+    }
+
+  });
+
+});
+
+cards.forEach((card) => {
+
+  card.style.opacity = "0";
+  card.style.transform = "translateY(40px)";
+  card.style.transition = "0.8s ease";
+
+});
